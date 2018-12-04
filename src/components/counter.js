bccb6d3a36c7d704a32cf5789ec265a2c08c561c
Owais Badat <owaisbadat@gmail.com>
2018-12-03 20:32:05 -0600

    first commit

---
+++ src/components/counter.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './counter.css'
+
+export class Counterbutton extends Component{
+
+    state = {counter: 0}
+
+    constructor(props) {
+        super(props);
+        this.incrementCount = this.incrementCount.bind(this)
+        this.decrementCount = this.decrementCount.bind(this)
+    }
+
+    incrementCount(){
+        this.setState(prevState => ({
+            counter: prevState.counter + 1
+        }))
+    }
+
+    decrementCount(){
+        this.setState(prevState => ({
+            counter: prevState.counter - 1
+        }))
+    }
+
+    render(){
+        return (
+          <div class='page'>
+            <div class='stepper'>
+              <h1>Step Counter</h1>
+            </div>
+            <div class='stepper'>
+                <button class='decrease' onClick={this.decrementCount}> - </button>
+                    <div class='count'>
+                        <p>{this.state.counter}</p>
+                    </div>
+                <button class='increase' onClick={this.incrementCount}> + </button>
+
+            </div>
+            </div>
+        )
+    }
+
+}
+
+export default Counterbutton
